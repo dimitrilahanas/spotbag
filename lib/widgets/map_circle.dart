b3032d5c8c2c8610df97c6fc7d2715af0af2f882
Dimitrios Lahanas <dimi@Dimitrioss-MacBook-Pro.local>
2025-11-26 21:24:35 +1100

    refactor(MapCircle): removed alignment and edited drop shadow

--- lib/widgets/map_circle.dart
+++ lib/widgets/map_circle.dart
@@ -4,28 +4,24 @@ class MapCircle extends StatelessWidget {
   final double height;
   final double width;
   final Color color;
-  final double posx;
-  final double posy;
 
-  const MapCircle(this.height, this.width, this.color, this.posx, this.posy, {super.key,});
+  const MapCircle(this.height, this.width, this.color, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment(posx, posy),
-      child: Container(
-        height: height,
-        width: width,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle, 
-          color: color,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 10,
-              color: Colors.black.withValues(alpha: 0.75),
-              spreadRadius: 4,
-          )],
-        ),
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.black.withValues(alpha: 0.75),
+            spreadRadius: 2,
+          ),
+        ],
       ),
     );
   }
